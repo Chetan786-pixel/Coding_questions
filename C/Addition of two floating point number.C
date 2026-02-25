5c48b1e195834cb24933d2d0687ba8506f89416e
Chetan786-pixel <chetan427816@gmail.com>
2026-02-25 21:27:53 +0530

    Add files via upload

---
+++ C/Addition of two floating point number.C
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	float num1,num2,sum;
+	scanf("%f",&num1);
+	scanf("%f",&num2);
+	sum= num1+num2;
+	printf("%.3f\n",sum);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
